ef8c0a8fe32457bb30e375ee27d55fe2a6be35a1
huangtao728 <970522851@qq.com>
2020-03-16 00:16:15 +0800

    second

---
+++ config.js
@@ -0,0 +1,35 @@
+// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: 'Miracle Board',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+    'ur256522-d26322d44f5a8e51c027e388',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: false,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 30,
+
+  // 导航栏菜单
+  Navi: [
+	{
+	  text: 'Home',
+	  url: 'https://status.jht.io/'
+	},
+	{
+	  text: 'GitHub',
+	  url: 'https://github.com/yb/uptime-status'
+	}
+  ]
+};
